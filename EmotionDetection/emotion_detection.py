437ccb98e02c15840a94dead56c8e16ea0cb5feb
dardourmariem <mariem.dardour@gmail>
2025-04-06 17:07:34 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,14 +5,24 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     jsonobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = jsonobj, headers=header)
-    print(response.text)
     formatted_response = json.loads(response.text)
+    print(response.status_code)
+
     try:
         if response.status_code == 400:
-            formatted_response = apply_none_condition(formatted_response['emotionPredictions'][0]['emotion'])
+            formatted_response = {
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+                }
         return formatted_response
     except requests.exceptions.RequestException as e:
         return {"error": f"An error occurred while making the request: {e}"}
+    return formatted_response
+
 
 def apply_none_condition(d):
     for key, value in d.items():
